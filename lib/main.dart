548f444ab1ddfb26bc8f6f0851aa9c60eb6f45ba
João Henrique <joaohenriquess3287@gmail.com>
2023-09-13 22:12:05 -0300

    feat: removing dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:movies_app/app.dart';
 import 'package:movies_app/views/discover.dart';
 import 'package:movies_app/views/movie_detail.dart';
 
-Future initDotEnv() async {
-  await dotenv.load(fileName: ".env");
-}
-
 main() async {
-  await initDotEnv();
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
